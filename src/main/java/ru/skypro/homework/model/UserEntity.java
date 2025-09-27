28e39d4333c4c0268a5a5d5c688960733f6a876f
Vladimirs Anufrijevs <vladimirs.anufrijevs@balticom.eu>
2025-09-27 18:49:34 +0300

    add Entities

---
+++ src/main/java/ru/skypro/homework/model/UserEntity.java
@@ -0,0 +1,39 @@
+package ru.skypro.homework.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ru.skypro.homework.dto.Role role;
+
+    private String image;
+}
+
